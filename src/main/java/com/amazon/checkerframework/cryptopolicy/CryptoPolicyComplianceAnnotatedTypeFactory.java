32ba8766820311c07181e589fe3283173258d882
geektoto <121338012+geektoto@users.noreply.github.com>
2022-06-27 13:18:31 +0000

    merges from trunk

--- src/main/java/com/amazon/checkerframework/cryptopolicy/CryptoPolicyComplianceAnnotatedTypeFactory.java
+++ src/main/java/com/amazon/checkerframework/cryptopolicy/CryptoPolicyComplianceAnnotatedTypeFactory.java
@@ -3,4 +3,7 @@ package com.amazon.checkerframework.cryptopolicy;
 import javax.lang.model.element.AnnotationMirror;
 import java.lang.annotation.Annotation;
 import java.util.List;
-import java.util.Se
+import java.util.Set;
+
+import com.amazon.checkerframework.cryptopolicy.qual.CryptoBlackListed;
+import com.amazon.chec
