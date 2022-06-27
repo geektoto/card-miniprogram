b94b37870ee549589eec2e58f382cf3efcf427f4
geektoto <121338012+geektoto@users.noreply.github.com>
2022-06-27 16:42:33 +0000

    fix checkbox

--- src/main/java/com/amazon/checkerframework/cryptopolicy/CryptoPolicyComplianceAnnotatedTypeFactory.java
+++ src/main/java/com/amazon/checkerframework/cryptopolicy/CryptoPolicyComplianceAnnotatedTypeFactory.java
@@ -6,4 +6,5 @@ import java.util.List;
 import java.util.Set;
 
 import com.amazon.checkerframework.cryptopolicy.qual.CryptoBlackListed;
-import com.amazon.chec
+import com.amazon.checkerframework.cryptopolicy.qual.CryptoPolicyBottom;
+import com.amazon.checkerframework.cryptopolicy.qual.CryptoWhite
