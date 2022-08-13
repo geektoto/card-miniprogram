a40ba66381601a9ea0f9c21541540cf9ff7add2d
geektoto <121338012+geektoto@users.noreply.github.com>
2022-08-13 21:46:34 +0000

    Add travis build config

--- src/main/java/com/amazon/checkerframework/cryptopolicy/CryptoPolicyComplianceChecker.java
+++ src/main/java/com/amazon/checkerframework/cryptopolicy/CryptoPolicyComplianceChecker.java
@@ -9,4 +9,5 @@ import org.checkerframework.common.value.ValueChecker;
  * Dummy class used by the checker framework to load the type factory.
  * Note that the CheckerFramework uses convention driven development by searching for a (x)Checker class and then
  * loading the (x)AnnotatedTypeFactory.
- * The checker does not
+ * The checker does not provide a default @SuppressWarnings but uses
+ * {@link com.amazon.checkerframework.cryptopolicy.qual.Suppr
