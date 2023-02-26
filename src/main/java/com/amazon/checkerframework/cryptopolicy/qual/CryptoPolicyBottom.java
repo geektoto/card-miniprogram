59918f98760e4d86bb8e0548ac3e30ab13f837e5
geektoto <121338012+geektoto@users.noreply.github.com>
2023-02-26 06:09:04 +0000

    Fixed link in README

--- src/main/java/com/amazon/checkerframework/cryptopolicy/qual/CryptoPolicyBottom.java
+++ src/main/java/com/amazon/checkerframework/cryptopolicy/qual/CryptoPolicyBottom.java
@@ -15,4 +15,6 @@ import org.checkerframework.framework.qual.TypeUseLocation;
  * Bottom annotation for {@link CryptoWhiteListed} and {@link CryptoBlackListed} without args.
  */
 @Documented
-@Retent
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@SubtypeOf({CryptoBlackListed
