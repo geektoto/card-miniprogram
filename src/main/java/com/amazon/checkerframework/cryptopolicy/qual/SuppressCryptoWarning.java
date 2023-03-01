f10386942f3c581fd2fcbc68ac40a4575ee06f78
geektoto <121338012+geektoto@users.noreply.github.com>
2023-03-01 10:05:47 +0000

    Fix null pointer exception

--- src/main/java/com/amazon/checkerframework/cryptopolicy/qual/SuppressCryptoWarning.java
+++ src/main/java/com/amazon/checkerframework/cryptopolicy/qual/SuppressCryptoWarning.java
@@ -1,4 +1,8 @@
 package com.amazon.checkerframework.cryptopolicy.qual;
 
 import java.lang.annotation.Documented;
-import java.lang.
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.checkerf
