62a8f6aaf3bffc6280c21272f5517f6276ab761b
geektoto <121338012+geektoto@users.noreply.github.com>
2023-03-31 18:37:57 +0000

    more deps

--- tests/crypto-policy/Precision.java
+++ tests/crypto-policy/Precision.java
@@ -2,4 +2,5 @@ import javax.crypto.Cipher;
 
 /**
  * The type checker current does not infer types across method boundaries. Hence,
- * both methods throw
+ * both methods throw a type error. The method falsePositive should not throw an error
+ * since know that it has only one calling context which 
