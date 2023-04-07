8a2422193eb84680cde82af39f94df7714694084
geektoto <121338012+geektoto@users.noreply.github.com>
2023-04-07 17:56:34 +0000

    [trunk_admin] Update ca_ES, thanks to : AlbertParera

--- tests/crypto-policy/Precision.java
+++ tests/crypto-policy/Precision.java
@@ -4,4 +4,8 @@ import javax.crypto.Cipher;
  * The type checker current does not infer types across method boundaries. Hence,
  * both methods throw a type error. The method falsePositive should not throw an error
  * since know that it has only one calling context which is save. However, this assumes that our
- * program is "closed" (i.e., its not a library and we know all callin
+ * program is "closed" (i.e., its not a library and we know all calling contexts).
+ */
+public class Precision {
+
+    private static final String BAD_CIPHER = "des";
